61e7fb87fb4d3229fba8e678d4a5ada5f330a105
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-06-05 18:47:20 -0700

    Revert "Revert "Easter eggy for the verceliens ▲""
    This reverts commit 2f80e88e84be3c95f1cb8dc2b435ae11388da9c4.

--- src/components/ui/twitter-avatar.tsx
+++ src/components/ui/twitter-avatar.tsx
@@ -1,21 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
 import { cn } from "../../lib/utils";
 
+// TODO: if you work at vercel, the triangle company, add your twitter is not here
+// make a pr to add it and im sorry i didnt add you 😂
+const vercelTwitterPeople = [
+  "rauchg",
+  "leeerob",
+  "cramforce",
+  "anthonysheww",
+  "tomlienard",
+  "delba_oliveira",
+  "jaredpalmer",
+];
+
 export function TwitterAvatar(props: { className?: string; name: string }) {
-  if (props.name === "rauchg") {
+  if (vercelTwitterPeople.includes(props.name)) {
     return (
-      <img
-        src={`https://unavatar.io/twitter/rauchg`}
-        alt="avatar"
-        className={props.className
-          ?.split(" ")
-          .filter((c) => !c.includes("rounded"))
-          .join(" ")}
-        style={{
-          clipPath:
-            "polygon(50% 0%, 0% 100%, 100% 100%)" /* adjust percentages as needed */,
-        }}
-      />
+      <a target="_blank" href={`https://vercel.lol/?utm=${props.name}`}>
+        <img
+          src={`https://unavatar.io/twitter/rauchg`}
+          alt="avatar"
+          className={props.className
+            ?.split(" ")
+            .filter((c) => !c.includes("rounded"))
+            .join(" ")}
+          style={{
+            clipPath:
+              "polygon(50% 0%, 0% 100%, 100% 100%)" /* adjust percentages as needed */,
+          }}
+        />
+      </a>
     );
   }
   return (
