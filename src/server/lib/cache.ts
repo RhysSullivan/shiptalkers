b9ed9175fbc436bc6f17b5669f78eb5b67511cde
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-06-05 18:37:51 -0700

    fix redis connection

--- src/server/lib/cache.ts
+++ src/server/lib/cache.ts
@@ -1,34 +1,21 @@
 import { RedisClientType, createClient } from 'redis';
 import { env } from '../../env';
-const redis = createClient({
+const client = createClient({
     url: env.REDIS_URL,
 });
-
+void client.connect();
 
 import superjson from 'superjson';
 
-let redisClient: RedisClientType;
-
-async function getRedisClient() {
-    if (!redisClient) {
-        redisClient = createClient();
-        await redisClient.connect();
-    } else if (!redisClient.isOpen) {
-        await redisClient.connect();
-    }
-    return redisClient;
-}
 
 
 export async function writeToCache<T>(key: string, value: T) {
-    const client = await getRedisClient();
     return client.set(key,
         superjson.stringify(value),
     );
 }
 
 export async function readFromCache<T>(key: string): Promise<T | null> {
-    const client = await getRedisClient();
     const value = await client.get(key);
     if (!value) {
         return null;
